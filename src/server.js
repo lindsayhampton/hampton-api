f48976d3b05564b02faf18b33b4afba0473baf50
Lindsay Hampton <lrhampton@fullsail.edu>
2016-07-29 20:01:22 -0400

    I had req and res mixed up

--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
+var port = 3000;
 
-app.listen(3000, function() {
+app.get('/', function(req,res){
+  res.json({ hello: 'world' });
+});
+
+app.listen(port, function() {
   console.log('our server is running on port 3000');
 });
